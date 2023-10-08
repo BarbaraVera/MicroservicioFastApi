c943ec5fef2338c1b2f2db92a1ad42f95b74f345
Barbara Vera <barbara.alejandra.v3@gmail.com>
2023-10-08 17:55:05 -0300

    endpoints de eliminar y actualizar tareas
    -Se creó endpoint de eliminar por id
    -Se creó endpoints para actualizar por put y patch

--- esquema.py
+++ esquema.py
@@ -3,6 +3,17 @@ from datetime import date
 from pydantic import BaseModel
 
 class Tarea_esquema (BaseModel):
+    id: int
     titulo: str
     descripcion:str
     fecha_vencimiento : date
+
+class Tarea_actualizada(BaseModel):
+    titulo: str
+    descripcion:str
+    fecha_vencimiento : date
+
+class Tarea_actualizada_patch(BaseModel):
+    titulo: str = None
+    descripcion:str = None
+    fecha_vencimiento : date = None
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from basededatos import SessionLocal, Tarea
-from esquema import Tarea_esquema
+from esquema import Tarea_esquema, Tarea_actualizada, Tarea_actualizada_patch
 
 app = FastAPI()
 
@@ -11,6 +11,7 @@ def crear_tarea(tarea: Tarea_esquema):
     conexion = SessionLocal()
 
     tarea_nueva = Tarea(
+        id = tarea.id,
         titulo=tarea.titulo,
         descripcion = tarea.descripcion,
         fecha_vencimiento = tarea.fecha_vencimiento
@@ -30,3 +31,60 @@ def obtener_tareas():
     tareas = conexion.query(Tarea).all()
     conexion.close()
     return tareas
+
+#eliminar tarea
+@app.delete("/tareas/{tarea_id}/")
+def eliminar_tarea(tarea_id: int):
+    conexion = SessionLocal()
+    tarea = conexion.query(Tarea).filter(Tarea.id == tarea_id).first()
+
+    if tarea is None:
+        conexion.close()
+        raise HTTPException(status_code=404, detail="No existe")
+    
+    conexion.delete(tarea)
+    conexion.commit()
+    conexion.close()
+
+    return {"message": "La tarea se ha eliminado"}
+
+#actualizar tareas
+@app.put("/tareas/{tarea_id}/")
+def actualizar_tarea(tarea_id: int, tarea_actualizada: Tarea_actualizada):
+    conexion = SessionLocal()
+    tarea = conexion.query(Tarea).filter(Tarea.id == tarea_id).first()
+
+    if tarea is None:
+        conexion.close()
+        raise HTTPException(status_code=404, detail="La tarea no existe")
+    
+    tarea.titulo = tarea_actualizada.titulo
+    tarea.descripcion = tarea_actualizada.descripcion
+    tarea.fecha_vencimiento = tarea_actualizada.fecha_vencimiento
+
+    conexion.commit()
+    conexion.close()
+
+    return {"message": "Tarea actualizada correctamente"}
+
+#actualizar con patch
+@app.patch("/tareas/{tarea_id}/")
+def actualizar_tarea_patch(tarea_id: int, tarea_actualizada: Tarea_actualizada_patch):
+    conexion = SessionLocal()
+    tarea = conexion.query(Tarea).filter(Tarea.id == tarea_id).first()
+
+    if tarea is None:
+        conexion.close()
+        raise HTTPException(status_code=404, detail="La tarea no existe")
+    
+    if tarea_actualizada.titulo is not None:
+        tarea.titulo = tarea_actualizada.titulo
+    if tarea_actualizada.descripcion is not None:
+        tarea.descripcion = tarea_actualizada.descripcion
+    if tarea_actualizada.fecha_vencimiento is not None:
+        tarea.fecha_vencimiento = tarea_actualizada.fecha_vencimiento
+    
+    conexion.commit()
+    conexion.close()
+
+    return  {'message': 'tarea actualizada'}
